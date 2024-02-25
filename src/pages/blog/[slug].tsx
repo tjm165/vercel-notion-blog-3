320a5aa1f91ef78e5c51a5decdbe6653d9262530
Thomas M <tjm165@case.edu>
2024-02-24 22:09:25 -0600

    [hotfix] Remove detrimental display_source encoding

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -345,9 +345,7 @@ const RenderPost = ({ post, redirect, preview }) => {
                 child = (
                   <Comp
                     key={!useWrapper ? id : undefined}
-                    src={encodeURIComponent(
-                      display_source as any
-                    )}
+                    src={display_source}
                     controls={!isImage}
                     alt={`An ${isImage ? 'image' : 'video'} from Notion`}
                     loop={!isImage}
