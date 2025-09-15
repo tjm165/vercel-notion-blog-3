89b8f14d40c92807ea96b44ca17469e6ebec71d6
TM <tom.moawad@gmail.com>
2025-09-14 23:44:24 -0500

    Finalize bio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,45 +17,50 @@ export default function Index() {
         <h1>Thomas Moawad</h1>
         <h2>Software Engineer</h2>
 
-        {/* <Features /> */}
-
         <h2>Bio</h2>
 
         <div className="explanation">
           <p>
-            I am a Senior Software Engineer 1, at{' '}
+            <strong>I am a Senior Software Engineer </strong> at{' '}
             <ExtLink href="https://www.mckinsey.com">
               McKinsey & Company
             </ExtLink>
-            {'.'} I am responsible for the Identity and Access Management
-            platform. This is the part of the security that revolves around the
-            two main questions: "Who are you?" and "What can you access?". These
-            two fundamental questions are quite complex at a global organization
-            that is centered around client confidentiality. One of my proudest
-            accomplishments was leading the initiative to rewrite our forgot
-            password functionality. I love the challenges that come in this
-            space, and the opportunity to make a meaningful impact on the
-            security posture of a large organization.
+            {'. '}I am responsible for the Identity and Access Management
+            platform. This is the part of the security that focuses on two core
+            questions:
+            <ol>
+              <li>Who are you?</li>
+              <li>What can you access?</li>
+            </ol>
+            These two questions are particularly complex for a global
+            organization whose top priority is client confidentiality. One of my
+            proudest accomplishments was leading the initiative to rewrite
+            McKinsey's forgot password functionality. I love the challenges in
+            this space because they let me dive deep into meaningful technology
+            problems while significantly improving the security posture of a
+            large organization.
           </p>
 
           <p>
-            I am also purusing my MBA at{' '}
+            <strong> I am also purusing my MBA </strong> at{' '}
             <ExtLink href="https://www.kellogg.northwestern.edu">
               The Northwestern University Kellogg School of Management
             </ExtLink>{' '}
-            with a focus in Strategy and Technology Management. I am excited to
-            pair my technical background with a business education to become a
-            more well-rounded leader in the tech space. I believe that this will
-            take me far.
+            with a focus in Strategy and Technology Management. My goal is to
+            combine my technical background with the lessons I have learned at
+            McKinsey and a structured business education. I believe that this
+            will take me far in a career in tech leadership.
           </p>
 
           <p>
-            I completed my B.S. in Computer Science at{' '}
+            <strong> I completed my B.S. in Computer Science </strong> at{' '}
             <ExtLink href="https://case.edu">
-              Case Western Reserve University,
+              Case Western Reserve University
+            </ExtLink>
+            {'. '}I served as the President of the{' '}
+            <ExtLink href="https://engineering.case.edu/news/spartan-showcase-featuring-tommy-moawad">
+              Case Engineers Council
             </ExtLink>{' '}
-            where I served as the President of the
-            <ExtLink href="https://engineering.case.edu/news/spartan-showcase-featuring-tommy-moawad"></ExtLink>{' '}
             and worked as a Resident Assistant (RA) for three years guiding
             freshmen students through their transition to college life.
           </p>
